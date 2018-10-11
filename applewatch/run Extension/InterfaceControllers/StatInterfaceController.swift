eecc84c0f7fb2205b50328b87c1707a42bf83174
Lauren McKenna <lmckenna@underarmour.com>
2018-10-11 13:07:30 -0500

    testing digital crown

--- applewatch/run Extension/InterfaceControllers/StatInterfaceController.swift
+++ applewatch/run Extension/InterfaceControllers/StatInterfaceController.swift
@@ -9,12 +9,19 @@
 import Foundation
 import WatchKit
 
-class StatInterfaceController: WKInterfaceController {
+class StatInterfaceController: WKInterfaceController, WKCrownDelegate {
     
+    private var crownAccumulator = 0.0
+
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         becomeCurrentPage()
+        crownSequencer.delegate = self
         // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        crownSequencer.focus()
     }
     
     @IBAction func didTapScreen(_ sender: Any) {
@@ -25,4 +32,21 @@ class StatInterfaceController: WKInterfaceController {
     @IBAction func didPressCancel() {
         WKInterfaceController.reloadRootControllers(withNames: ["InterfaceController"], contexts: nil)
     }
+    
+    func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
+        crownAccumulator += rotationalDelta
+        
+        if crownAccumulator > 0.1 {
+            let names = ["ControlsInterface", "HRInterface", "NowPlayingInterface"]
+            WKInterfaceController.reloadRootPageControllers(withNames: names, contexts: nil, orientation: .horizontal, pageIndex: 1)
+            resetAccumulator()
+        } else if crownAccumulator < -0.1 {
+            resetAccumulator()
+        }
+    }
+    
+    private func resetAccumulator() {
+        crownAccumulator = 0.0
+    }
+
 }
